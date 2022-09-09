3b0c4411bed94a40ff754e793f67a68ed55b771e
Hardik Brahmbhatt <Hrkbrahmbhatt@gmail.com>
2022-09-08 23:34:58 -0400

    jest test

--- WeatherApi/index.test.js
+++ WeatherApi/index.test.js
@@ -4,9 +4,34 @@ const axios = require("axios");
 jest.mock("axios");
 
 test("Temprature Test Case", async () => {
-  const lat = 42.47;
-  const lng = 50.51;
-  const data = { temprature: 15 };
+  const lat = 60.56;
+  const lng = 50.5;
+  const data = {
+    coord: { lon: 60.56, lat: 50.5 },
+    weather: [
+      { id: 804, main: "Clouds", description: "overcast clouds", icon: "04d" },
+    ],
+    base: "stations",
+    main: {
+      temp: 283.98,
+      feels_like: 282.24,
+      temp_min: 283.98,
+      temp_max: 283.98,
+      pressure: 1020,
+      humidity: 43,
+      sea_level: 1020,
+      grnd_level: 986,
+    },
+    visibility: 10000,
+    wind: { speed: 4.01, deg: 249, gust: 7.6 },
+    clouds: { all: 97 },
+    dt: 1662693976,
+    sys: { country: "RU", sunrise: 1662686626, sunset: 1662733622 },
+    timezone: 18000,
+    id: 1490501,
+    name: "Svetlyy",
+    cod: 200,
+  };
   axios.get.mockResolvedValue({ data });
   const weather = await weatherApi.getWeatherinfo(lat, lng);
   expect(weather).toBe(data);
--- geoLocation/index.test.js
+++ geoLocation/index.test.js
@@ -4,11 +4,66 @@ const axios = require("axios");
 jest.mock("axios");
 
 test("Location Test Case ", async () => {
-  const lat = 42.47;
-  const lng = 50.51;
-  const results = { lat, lng };
-  const data = { results };
+  const data = {
+    results: [
+      {
+        address_components: [
+          {
+            long_name: "Toronto",
+            short_name: "Toronto",
+            types: ["locality", "political"],
+          },
+          {
+            long_name: "Toronto",
+            short_name: "Toronto",
+            types: ["administrative_area_level_2", "political"],
+          },
+          {
+            long_name: "Ontario",
+            short_name: "ON",
+            types: ["administrative_area_level_1", "political"],
+          },
+          {
+            long_name: "Canada",
+            short_name: "CA",
+            types: ["country", "political"],
+          },
+        ],
+        formatted_address: "Toronto, ON, Canada",
+        geometry: {
+          bounds: {
+            northeast: {
+              lat: 43.8554579,
+              lng: -79.1168971,
+            },
+            southwest: {
+              lat: 43.5810245,
+              lng: -79.639219,
+            },
+          },
+          location: {
+            lat: 43.653226,
+            lng: -79.3831843,
+          },
+          location_type: "APPROXIMATE",
+          viewport: {
+            northeast: {
+              lat: 43.8554579,
+              lng: -79.1168971,
+            },
+            southwest: {
+              lat: 43.5810245,
+              lng: -79.639219,
+            },
+          },
+        },
+        place_id: "ChIJpTvG15DL1IkRd8S0KlBVNTI",
+        types: ["locality", "political"],
+      },
+    ],
+    status: "OK",
+  };
   axios.get.mockResolvedValue({ data });
   const address = await geoLocation.getAddress("Toronto");
-  expect(JSON.stringify(address)).toBe(JSON.stringify(results));
+  expect(JSON.stringify(address)).toBe(JSON.stringify(data.results));
 });
